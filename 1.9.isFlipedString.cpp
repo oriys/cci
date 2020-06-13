f2b335c4c725654262126562269ac2436e4912b3
oriys <kirin.toudou@protonmail.com>
2020-06-14 05:30:45 +0800

    [ADD] 1.9

---
+++ 1.9.isFlipedString.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    bool isFlipedString(string s1, string s2)
+    {
+        return s1.size() == s2.size() && (s1 + s1).find(s2) != -1;
+    }
+};
